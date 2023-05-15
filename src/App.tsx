d6074694833e35f9ec471ce78b6f6ebe3e02c119
Matthijs <info@matsmedia.nl>
2023-05-15 19:07:12 +0200

    added eslint, wip scroll component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import styles from './App.module.css'
-import {Header, Banner} from './components';
+import {Header, Banner, Test} from './components';
 import classnames from 'classnames';
 import { HomeView } from './Views';
 
 const cx = classnames.bind(styles)
 
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
 
   return (
     <div className="App">
-      <Header />
-      <Banner />
+      {/* <Header />
+      <Banner /> */}
       <HomeView />
+
     </div>
   )
 }
--- src/Views/HomeView.tsx
+++ src/Views/HomeView.tsx
@@ -2,32 +2,40 @@ import React from 'react';
 import styles from './HomeView.module.css';
 import classnames from 'classnames';
 import { Socials } from '../components/Socials';
+import { Test } from '../components';
+
+
 
 const cx = classnames.bind(styles)
 
 export const HomeView = () => {
-  const test = 'hoi'
+
+const sections = [
+  <Socials />,
+  <div className={cx(styles.about)}>
+    <div className={cx(styles.item)}>
+      <h4>About me</h4>
+      <span>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, adipisci amet veniam sed velit consec
+      </span>
+    </div>
+    <div className={cx(styles.item)}>
+      <h4>Career</h4>
+      <span>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, adipisci amet veniam sed velit consec
+      </span>
+    </div>
+  </div>
+]
+
+
   return (
     <div className={cx(styles.base)}>
       <div className={cx(styles.typewriter)}>
         <h1>Hello world</h1>
       </div>
-        <h3>Welcome at the personal site of Matthijs Beeke or Matty for friends</h3>
-        <div className={cx(styles.about)}>
-          <div className={cx(styles.item)}>
-            <h4>About me</h4>
-            <span>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, adipisci amet veniam sed velit consec
-            </span>
-          </div>
-          <div className={cx(styles.item)}>
-            <h4>Career</h4>
-            <span>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, adipisci amet veniam sed velit consec
-            </span>
-          </div>
-        </div>
-        <Socials />
-      </div>
+      <h3>Welcome on the personal site of Matthijs Beeke or Matty for friends</h3>
+      <Test sections={sections}/>
+    </div>
   )
 }
